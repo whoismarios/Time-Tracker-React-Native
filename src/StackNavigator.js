7225b854f9f45352af4480a2964fff403684f3ae
Marios Tzialidis <marios@tzialidis.com>
2023-05-21 22:38:47 +0200

    Icons added to the Tab.Navigator. Active Tab is Orange. Inactive Blue

--- src/StackNavigator.js
+++ src/StackNavigator.js
@@ -6,6 +6,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './screens/HomeScreen/HomeScreen';
 import WorkScreen from './screens/WorkScreen/WorkScreen';
 import StatisticsScreen from './screens/StatisticsScreen/StatisticsScreen';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -25,7 +26,28 @@ const MainStack = () => {
 const AppNavigator = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+
+            if (route.name === 'Main') {
+              iconName = 'home';
+            } else if (route.name === 'Work') {
+              iconName = 'briefcase';
+            } else if (route.name === 'Statistics') {
+              iconName = 'pie-chart';
+            }
+
+            return <Icon name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'orangered',
+          tabBarInactiveTintColor: 'rgb(0, 143, 209)',
+          tabBarStyle: {
+            display: 'flex',
+          },
+        })}
+      >
         <Tab.Screen name="Main" component={MainStack} />
         <Tab.Screen name="Work" component={WorkScreen} />
         <Tab.Screen name="Statistics" component={StatisticsScreen} />
